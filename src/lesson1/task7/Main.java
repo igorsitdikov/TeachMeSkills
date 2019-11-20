7beb162e5f936665f9e2030083a02ee389db7a46
sitdikov <l2m>
2019-11-20 09:09:23 +0300

    add homework lesson 1; only switch in task 7;

--- src/lesson1/task7/Main.java
+++ src/lesson1/task7/Main.java
@@ -4,30 +4,33 @@ public class Main {
 
     public static void main ( String[] args ) {
         int numProgrammers = 121;
-        if (numProgrammers % 100 > 10 && numProgrammers % 100 < 20) {
-            System.out.println(numProgrammers + " программистов");
-        } else {
-            switch (numProgrammers % 10) {
-                case 1:
-                    System.out.println(numProgrammers + " программист");
-                    break;
-                case 2:
-                case 3:
-                case 4:
-                    System.out.println(numProgrammers + " программиста");
-                    break;
-                default:
-                    System.out.println(numProgrammers + " программистов");
+
+        switch (numProgrammers % 100) {
+            case 11:
+            case 12:
+            case 13:
+            case 14:
+            case 15:
+            case 16:
+            case 17:
+            case 18:
+            case 19:
+                System.out.println(numProgrammers + " программистов");
+                break;
+            default: {
+                switch (numProgrammers % 10) {
+                    case 1:
+                        System.out.println(numProgrammers + " программист");
+                        break;
+                    case 2:
+                    case 3:
+                    case 4:
+                        System.out.println(numProgrammers + " программиста");
+                        break;
+                    default:
+                        System.out.println(numProgrammers + " программистов");
+                }
             }
         }
-//        if ((numProgrammers % 100) > 10 && (numProgrammers % 100) < 20) {
-//            System.out.println(numProgrammers + " программистов");
-//        } else if (numProgrammers % 10 == 1) {
-//            System.out.println(numProgrammers + " программист");
-//        } else if (numProgrammers % 10 == 2 || numProgrammers % 10 == 3 || numProgrammers % 10 == 4) {
-//            System.out.println(numProgrammers + " программиста");
-//        } else {
-//            System.out.println(numProgrammers + " программистов");
-//        }
     }
 }
