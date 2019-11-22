b99be8459512042cebe5cea4d43a33260547b8ef
lissen2me <lissen2me@yandex.ru>
2019-11-22 22:48:22 +0300

    lesson2 task1 - complete; changed gitignore;

--- src/lesson2/task1/Main.java
+++ src/lesson2/task1/Main.java
@@ -2,13 +2,12 @@ package lesson2.task1;
 
 public class Main {
     public static void main(String[] args) {
-        double distance = 10;
+        double dailyDistance = 10;
         double fullDistance = 0;
         for (int i = 0; i < 7; i++) {
-            fullDistance += distance;
-            distance +=  distance * 0.1;
+            fullDistance += dailyDistance;
+            dailyDistance +=  dailyDistance * 0.1;
         }
-        System.out.println(distance);
-        System.out.println(fullDistance);
+        System.out.println("Путь за 7 дней : " + fullDistance);
     }
 }
