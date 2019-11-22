d432a12d011390fd193a8a32707f20634a4dce33
lissen2me <lissen2me@yandex.ru>
2019-11-22 23:01:03 +0300

    lesson2 task4 - complete;

--- src/lesson2/task4/Main.java
+++ src/lesson2/task4/Main.java
@@ -2,12 +2,12 @@ package lesson2.task4;
 
 public class Main {
     public static void main(String[] args) {
-        int A = 10;
-        int B = 100;
+        int A = 3;
+        int B = 3;
         int result = 0;
         for (int i = 0; i < A; i++) {
             result += B;
         }
-        System.out.println(result);
+        System.out.println("A*B : " + result);
     }
 }
