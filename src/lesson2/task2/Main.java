d259efb1baa6b795ddd44458557bfe6b8a3c188d
lissen2me <lissen2me@yandex.ru>
2019-11-22 23:10:45 +0300

    lesson2 task2 - complete;

--- src/lesson2/task2/Main.java
+++ src/lesson2/task2/Main.java
@@ -3,7 +3,7 @@ package lesson2.task2;
 public class Main {
     public static void main(String[] args) {
         int ameba = 1;
-        for (int i = 0; i < 12; i+=3) {
+        for (int i = 0; i <= 24; i+=3) {
             System.out.println("Hour " + i + " ameba : " + ameba);
             ameba *= 2;
         }
