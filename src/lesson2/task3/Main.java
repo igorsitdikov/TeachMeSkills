562f4be14aceca8801c2a34fccf60db0f383aab1
lissen2me <lissen2me@yandex.ru>
2019-11-22 23:06:19 +0300

    lesson2 task3 - complete;

--- src/lesson2/task3/Main.java
+++ src/lesson2/task3/Main.java
@@ -4,9 +4,14 @@ public class Main {
     public static void main(String[] args) {
         int sum = 0;
         for (int i = 1; i <= 256; i *= 2) {
-            System.out.println(i);
+
+            if (i == 1)
+                System.out.print(i);
+            else
+                System.out.print("+" + i);
+
             sum += i;
         }
-        System.out.println("Sum = " + sum);
+        System.out.println(" = " + sum);
     }
 }
