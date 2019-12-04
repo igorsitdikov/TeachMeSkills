55c1887265bf140e690fd377184469ff137884e4
mizuro <lissen2me@yandex.ru>
2019-12-04 20:49:29 +0300

    static;

--- src/class_tasks/lesson5/Lesson5.java
+++ src/class_tasks/lesson5/Lesson5.java
@@ -12,13 +12,32 @@ public class Lesson5 {
         System.out.println();
 
         Student student = new Student("John", "Smith", 23);
+        student.birthday();
+        student.doHomework(0);
+        student.doHomework(1);
+        student.doHomework(2);
+        student.doHomework(3);
+        student.doHomework(4);
+        student.doHomework(5);
+        student.doHomework(6);
+        student.doHomework(7);
+        student.doHomework(8);
+        student.doHomework(9);
+//        student.doHomework(10);
+        System.out.println("Выполненные задания");
+        for (int i = 0; i < student.getHomeworkDone().length ; i++) {
+            System.out.print(student.getHomeworkDone()[i] + " ");
+        }
+        System.out.println();
         student.printInformation();
         System.out.println(student.getFullName());
         System.out.println(student.getFullName("Mr."));
         Student[] students = new Student[2];
         students[0] = new Student("Bill", "Klinton", 34);
         students[1] = new Student("Jim", "Kerry", 22);
+        students[1].doHomework(2);
         Group group = new Group(1, students, "Python");
+        Group group1 = new Group(2, students, "Python");
         System.out.println("Language previous: " + group.getProgramLanguage());
         group.setId(1);
         group.addStudent(student);
@@ -27,9 +46,18 @@ public class Lesson5 {
         System.out.println();
         System.out.println("ID group : " + group.getId());
         System.out.println("Language : " + group.getProgramLanguage());
-        System.out.println("Students : ");
-        for (int i = 0; i < group.getStudents().length; i++) {
-            System.out.println("\t" + group.getStudents()[i].getFullName());
+
+        group.printInfomation();
+
+        System.out.println("Выполнили задание : ");
+        Student[] st = group.checkHomework(2);
+        for (int i = 0; i < st.length; i++) {
+            st[i].printInformation();
         }
+
+        System.out.println("Количество студентов : " + Student.getNumberOfStudents());
+
+        Group[] groups = group.getGroups();
+        System.out.println();
     }
 }
--- src/class_tasks/lesson5/model/Student.java
+++ src/class_tasks/lesson5/model/Student.java
@@ -3,9 +3,12 @@ package class_tasks.lesson5.model;
 import java.util.Scanner;
 
 public class Student {
+    static int count = 0;
     private String firstName;
     private String lastName;
     private int age;
+    private boolean[] homework = new boolean[10];
+    private int[] homeworksDone = new int[0];
 
     public Student() {
         System.out.println("Enter first name");
@@ -15,20 +18,46 @@ public class Student {
         this.lastName = scanner.nextLine();
         System.out.println("Enter age");
         this.age = scanner.nextInt();
+        count++;
     }
 
     public Student(String firstName, String lastName, int age) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.age = age;
+        count++;
     }
 
     public void printInformation() {
-        System.out.println("first name : " + this.firstName + "\nlast name : " + this.lastName + "\nage : " + this.age);
+        System.out.println(getFullName() + "\nage : " + this.age);
+    }
+
+    public void doHomework(int id) {
+        if (homeworksDone.length >= 10) {
+            System.out.println("Студент устал((");
+        } else {
+            this.homework[id] = true;
+            int[] homeworksBuf = new int[this.homeworksDone.length + 1];
+            for (int i = 0; i < this.homeworksDone.length; i++) {
+                homeworksBuf[i] = this.homeworksDone[i];
+            }
+            homeworksBuf[this.homeworksDone.length] = id;
+            this.homeworksDone = homeworksBuf;
+        }
+    }
+    public static int getNumberOfStudents(){
+        return count;
+    }
+    public int[] getHomeworkDone() {
+        return this.homeworksDone;
+    }
+
+    public void birthday() {
+        this.age++;
     }
 
     public String getFullName() {
-        return this.firstName + " " + this.lastName;
+        return "first name : " + this.firstName + "\nlast name : " + this.lastName;
     }
 
     public String getFullName(String prefix) {
