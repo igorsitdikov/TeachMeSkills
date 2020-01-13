8d11bd06d70452b5fab86e4c96c41cf302fa066a
Igor Sitdikov <ihar.sitdzikau@yandex.by>
2020-01-13 23:40:51 +0300

    lesson 11 hometask completed

--- src/lesson11/Main.java
+++ src/lesson11/Main.java
@@ -6,6 +6,7 @@ import lesson10.Car;
 import lesson9.task4.TextFormatter;
 
 import java.io.*;
+import java.net.SocketTimeoutException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -23,12 +24,12 @@ public class Main {
         String text = stringBuilder.toString().replace('\n', ' ');
         String[] sentences = text.split("(?<=(\\.|\\?|!))");
         stringBuilder.setLength(0);
-        for (int i = 0; i < sentences.length; i++) {
-            if (TextFormatter.hasPolindrom(sentences[i].trim())
-                    || (TextFormatter.counter(sentences[i].trim()) > 2
-                    && TextFormatter.counter(sentences[i].trim()) < 6)) {
-                stringBuilder.append(sentences[i]).append('\n');
-                listSource.add(sentences[i]);
+        for (String s : sentences) {
+            if (TextFormatter.hasPolindrom(s.trim())
+                    || (TextFormatter.counter(s.trim()) > 2
+                    && TextFormatter.counter(s.trim()) < 6)) {
+                stringBuilder.append(s).append('\n');
+                listSource.add(s);
             }
         }
         BufferedWriter writer = new BufferedWriter(new FileWriter("src/lesson11/OUTPUT.TXT"));
@@ -49,47 +50,76 @@ public class Main {
 
         for (String item : listSource) {
             for (String blackWord : blacklist) {
-                if (item.contains(blackWord)){
+                if (item.contains(blackWord)) {
                     blackSentences.add(item);
                 }
             }
         }
 
-        if (blackSentences.size() != 0){
+        if (blackSentences.size() != 0) {
             System.out.println("Количество предложений со словами из черного списка : " + blackSentences.size());
             System.out.println("Предложения, подлежащие проверке : ");
-            for (String sentence : blackSentences ) {
+            for (String sentence : blackSentences) {
                 System.out.println(sentence);
             }
         } else {
             System.out.println("Текст прошел проверку");
         }
-//        System.out.println(firstText);
-        try(ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("out.txt"))){
+        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("out.txt"))) {
+            List<Car> cars = new ArrayList<>();
+
             Car audi = new Car("Audi", 220, 15000);
             Car bmw = new Car("BMW", 320, 25000);
+            cars.add(audi);
+            cars.add(bmw);
+            oos.writeObject(cars);
             oos.writeObject(audi);
             oos.writeObject(bmw);
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
         }
 
-        try( ObjectInputStream ois = new ObjectInputStream(new FileInputStream("out.txt"))){
-            Car car = null;
-            while (( car = (Car) ois.readObject()) != null){
-//                Car car = (Car) ois.readObject();
+        try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream("out.txt"))) {
+
+            List<Car> cars = (List<Car>) ois.readObject();
+            for (Car car : cars) {
                 System.out.println(car.getModel());
                 System.out.println(car.getSpeed());
                 System.out.println(car.getPrice());
             }
+            try
+            {
+                for (;;)
+                {
+                    Car object = (Car) ois.readObject();
+                    System.out.println(object.getModel());
+                    System.out.println(object.getSpeed());
+                    System.out.println(object.getPrice());
+                }
+            }
+            catch (SocketTimeoutException exc)
+            {
 
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (ClassNotFoundException e) {
+            }
+            catch (EOFException exc)
+            {
+
+            }
+            catch (IOException exc)
+            {
+                exc.printStackTrace();
+            }
+
+//            Car audi = (Car) ois.readObject();
+//            Car bmw = (Car) ois.readObject();
+//            System.out.println(audi.getModel());
+//            System.out.println(audi.getSpeed());
+//            System.out.println(audi.getPrice());
+//            System.out.println(bmw.getModel());
+//            System.out.println(bmw.getSpeed());
+//            System.out.println(bmw.getPrice());
+
+        } catch (IOException | ClassNotFoundException e) {
             e.printStackTrace();
         }
 
