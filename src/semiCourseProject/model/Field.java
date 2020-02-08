d41271770a19d5c31742d33f66d4d6345e284071
sitdikov <lissen2me@yandex.ru>
2020-02-09 00:03:06 +0300

    fixed field

--- src/semiCourseProject/model/Field.java
+++ src/semiCourseProject/model/Field.java
@@ -1,9 +1,9 @@
 package semiCourseProject.model;
 
 public class Field {
-    private Figure[] field;
+    private Figure[][] field;
 
     public Field() {
-        field = new Figure[3];
+        field = new Figure[3][3];
     }
 }
